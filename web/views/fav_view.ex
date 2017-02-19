5fbcc11429c0333c81b689e3486c3e06982a9c3b
Xavier Serrano <Xavier Serrano>
2017-02-19 21:59:55 +0100

    i18n

--- web/views/fav_view.ex
+++ web/views/fav_view.ex
@@ -2,7 +2,7 @@ defmodule PokedexApi.FavView do
   use PokedexApi.Web, :view
   
   def render("limit_exceeded.json", _assigns) do
-    %{errors: [%{message: "Límite excedido de favoritos"}], type: "limit_exceeded"}
+    %{errors: [%{message: "Límite excedido de favoritos. Sólo puedes tener 10 marcados"}], type: "limit_exceeded"}
   end
 
 end
